651aac1319bd4ba65f2c681406fd3b85245e6f50
Samuel Agbonkpolo <samuelagm@gmail.com>
2020-08-15 05:41:53 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/samuelagm/prime-num-api
+
+go 1.13
+
+require (
+	github.com/Azorej/go-prime-generator v0.0.0-20150609172609-0baea3849902
+	github.com/labstack/echo/v4 v4.1.16
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+
+	primes "github.com/Azorej/go-prime-generator"
+)
+
+// Response object format
+type Response struct {
+	PrimeNumber int `json:"prime_number"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, &Response{PrimeNumber: handleRequest()})
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func handleRequest() int {
+	count := 10000000
+	primes := generate(count, time.Second*2)
+	r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
+	index := r1.Intn(len(primes))
+	return primes[index]
+}
+
+func generate(maxN int, timeout time.Duration) []int {
+	ch := make(chan int, 100)
+	enough := primes.NewChanSignal()
+
+	go primes.Generate(maxN, ch, enough)
+
+	timer := time.NewTimer(timeout)
+	res := make([]int, 0)
+
+	for {
+		select {
+		case v, more := <-ch:
+			if !more {
+				return res
+			}
+			res = append(res, v)
+
+		case <-timer.C:
+			enough <- primes.NewSignal()
+			println("Timeout :(")
+			return res
+		}
+	}
+}
